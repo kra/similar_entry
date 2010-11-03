c488c933008e9726f8f820e8f9083e47c885d775
Karl Anderson <kra@monkey.org>
2010-11-03 14:53:10 -0700

    renamed

--- calais.py
+++
@@ -1,8 +0,0 @@
-# http://code.google.com/p/python-calais/
-import calais
-
-def get_entities(text, api_key):
-    return [
-        entity['name']
-        for entity in calais.Calais(api_key, submitter="foo").analyze(text)]
-
