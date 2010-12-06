ce9665e18302bb4d9ea268563e3fc85ccf34615c
Karl Anderson <kra@monkey.org>
2010-12-05 18:57:58 -0800

    more users, top level run

--- run.py
+++ run.py
@@ -6,19 +6,55 @@ import operator
 import datetime
 
 def main():
-    users = {'karlanderson':entry.User('karlanderson'),
-             'blaine':entry.User('blaine'),
-             'al3x':entry.User('al3x'),
-             'nelson':entry.User('nelson'),
-             'pennjilette':entry.User('pennjilette'),
-             'theteller':entry.User('theteller')             
-             }
+    users = dict(
+        [(name, entry.User(name)) for name in
+         ['karlanderson',
+          'blaine',
+          'al3x',
+          'nelson',
+          'pennjillette',
+          'theteller',
+          'NASA_Ames',
+          'ebertchicago',
+          'hotdogsladies',
+          'beatonna',
+          'BPglobalPR',
+          'tgammons',
+          'jeresig',
+          'uncleboatshoes',
+          'mixedfeelings',
+          'portlandmercury',
+          'ourpdx',
+          'CristopOConnor',
+          'timbray',
+          'FrontAve',
+          'jennjoysmith',
+          'brx0',
+          'nothstine',
+          'Paxochka',
+          'erlsn',
+          'tomtomorrow',
+          'markos',
+          'gabrielamadeus',
+          'declanm',
+          'lukedones',
+          'kristyaudrey',
+          'tlockney',
+          'gfish',
+          'queeniecarly',
+          'adamd']])
     posts = {}
 
     while True:
         util.log('starting: posts:%s users:%s' % (len(posts), len(users)))
+        # Ideally, we should be doing this asynchronously or something - always
+        # be looking for new posts, always be looking for similar posts to what
+        # we have.  Might need basic scheduling to do this right, though, want
+        # to prioritize looking for similars for posts which haven't been
+        # checked in a while.
         for user in users.values():
-            for post in user.recent_posts():
+            for post in (
+                post for post in user.recent_posts() if not post.is_rt()):
                 if not posts.has_key(post.post_id):
                     posts[post.post_id] = post
             for post in sorted(posts.values(),
@@ -31,7 +67,6 @@ def main():
                         util.log('similar: %s' % similar_post.text)
                         if not posts.has_key(similar_post.post_id):
                             posts[post.post_id] = post
-                        import pdb; pdb.set_trace()
                         if not users.has_key(similar_post.from_user.username):
                             users[similar_post.from_user.username] = (
                                 similar_post.from_user)
